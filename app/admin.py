131ef9c554090e28a980b48c0e6b60c7554025db
Anael Barbosa <anael.noki@gmail.com>
2024-03-26 11:10:51 -0300

    feat: cria ``models.py`` e migrações
    Esse commit adiciona as classes model com base no levantamento de requisitos do sistema, também adiciona os models ao admin do Django e faz as migrações necessárias.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Product)
+admin.site.register(Institution)
+
+# Cria formuçários inline no admin do Django, apenas para testes 
+class CallProductInline(admin.TabularInline):
+    model = CallProduct
+    extra = 1
+
+class CallAdmin(admin.ModelAdmin):
+    inlines = [CallProductInline]
+
+admin.site.register(Call, CallAdmin)
+
+class OrderedProductInline(admin.TabularInline):
+    model = OrderedProduct
+    extra = 1
+    #  Função que associa Produtos da Chamada  com a Chamada do Pedido, apenas para testes no admin do Django
+    def get_formset(self, request, obj=None, **kwargs):
+      formset = super().get_formset(request, obj, **kwargs)
+      if obj:
+          formset.form.base_fields['call_product'].queryset = CallProduct.objects.filter(call=obj.call)
+      return formset
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderedProductInline]
+    
+admin.site.register(Order, OrderAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Institution(models.Model):
+    name = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Call(models.Model):
+    number = models.CharField( max_length=50)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    start = models.DateField(auto_now=False, auto_now_add=False)
+    end = models.DateField(auto_now=False, auto_now_add=False)
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Chamada - {self.number}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    unit = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class CallProduct(models.Model):
+    call = models.ForeignKey(Call, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    balance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.product} {self.price} - {self.call.number}'
+
+class Order(models.Model):
+    CHOICES = [
+        ('pending', 'Pendente'),
+        ('approved', 'Aprovado'),
+        ('delivered', 'Entregue'),
+    ]
+
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    call = models.ForeignKey(Call, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    status = models.CharField(max_length=10, choices=CHOICES, default='pending')
+
+    def __str__(self):
+        return f'{self.user} - {self.institution} - {self.call.number}'
+
+class OrderedProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    call_product = models.ForeignKey(CallProduct, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0) 
+
+    def __str__(self):
+        return f'({self.order}) ({self.call_product}) ({self.quantity})'
+    
