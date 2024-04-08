1fdc1c117c17b3971cd97103b31d636aee7d08f8
wenderson <110772284+wendersonvibes@users.noreply.github.com>
2024-04-07 21:23:00 -0300

    feat: cria modais com os formulários de cadastros
    Ainda falta organizar o css de alguns elementos e estilizar outros

--- static-templates/script.js
+++ static-templates/script.js
@@ -2,3 +2,10 @@ const toggler = document.querySelector(".btn");
 toggler.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("collapsed");
 });
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
