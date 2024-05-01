08d85abdd1629d08c789f7cd16233206f2492fb2
wenderson <110772284+wendersonvibes@users.noreply.github.com>
2024-05-01 09:36:29 -0300

    fix: adiciona máscara no input de cnpj (#11)
    Também adiciona pequenos detalhes para melhorar a experiência do usuário:
    	- Quando o passa o cursor do mouse em campos select, o campo fica com a borda preta e o cursor vira uma mão;
    	- Troca nas cores dos botões "cadastrar" dos modais, de azul para verde, porque o azul estava estranho;

--- static-templates/script.js
+++ static-templates/script.js
@@ -1,11 +1,14 @@
+// Sidebar toggler
 const toggler = document.querySelector(".btn");
 toggler.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("collapsed");
 });
 
+// Modal
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+
