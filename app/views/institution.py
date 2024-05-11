6f3c4b1444060bf2843241b99c5a63a1b95f2ddc
poliana-dev <m.polisys02@gmail.com>
2024-05-11 12:06:03 -0300

    feat: Detalhamento de instituição

--- app/views/institution.py
+++ app/views/institution.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from app.forms import InstitutionForm
-from app.models import Institution
+from app.models import Institution, UserProfile
 from app.utils.mixins import StaffRequiredMixin
 
 from ajax.views import AjaxListView, AjaxCreateView, AjaxUpdateView, AjaxDeleteView
@@ -38,4 +41,18 @@ class InstitutionDelete(AjaxDeleteView):
     success_url = reverse_lazy('institution-list')
     message = "Instituição DELETADO com sucesso!"
     message_class = "alert-primary"
+
+@login_required
+def InstitutionDetail(request, pk):
+    template_name = 'institution/detail.html'
+    context = {}
+    user = request.user
+    institution = get_object_or_404(Institution, pk=pk)
+
+    if (not user.is_staff) and (user.institution != institution):
+        messages.warning(request, "Você não tem acesso a essa Instituição.")
+        return redirect('institution-list')
+    
+    context['institution'] = institution
+    return render(request, template_name, context)
 
