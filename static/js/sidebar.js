9cf85d49ed0ed291faca73afb9a5c5fa538a1ee2
dvanael <anael.noki@gmail.com>
2024-05-15 00:23:38 -0300

    refactor: altera sidebar no html
    Esse commit altera como a sidebar é renderizada e reorganiza arquivos static
    
    Agora a sidebnar é renderizada apenas para usuários admin, no caso de
    usuário comum é renderizado a navbar mais simples. Isso permite utilizar
    os mesmos templates para diferentes tipos de usuários, como as paginas
    de Pedidos e Chamadas

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -3,12 +3,3 @@ const toggler = document.querySelector(".btn");
 toggler.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("collapsed");
 });
-
-// Modal
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
-
