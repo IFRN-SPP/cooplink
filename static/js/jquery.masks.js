988d3ca91d59ba6568834e50547535473ac90b54
dvanael <anael.noki@gmail.com>
2024-05-17 14:46:02 -0300

    refactor: muda ``cnpj_mask`` para ``id_cnpj``
    Altera como é feito a  busca o campo de cnpj e altera o nome do arquivo .js para
    evitar erros no console

--- static/js/jquery.masks.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $('.cnpj-mask').mask('00.000.000/0000-00');
-});
---
+++ static/js/jqueryMasks.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    if ($('#id_cnpj').length) {
+        $('#id_cnpj').mask('00.000.000/0000-00');
+    }
+});
