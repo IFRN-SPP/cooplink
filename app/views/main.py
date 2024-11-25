58d0d85990efc01bce3d48561af63094677ff84b
Ana Barbosa <anael.noki@gmail.com>
2024-11-25 09:17:52 -0300

    style: formatação padrão black no main.py

--- app/views/main.py
+++ app/views/main.py
@@ -8,38 +8,38 @@ from app.models import Order, Call, CallProduct
 
 @login_required
 def index(request):
-    template_name = 'index.html'
+    template_name = "index.html"
     context = {}
     user = request.user
 
     if user.is_staff:
-        return redirect('index-admin')
+        return redirect("index-admin")
 
     orders = Order.objects.filter(institution=user.institution)[:5]
-    call= Call.objects.filter(active=True, institution=user.institution).first()
+    call = Call.objects.filter(active=True, institution=user.institution).first()
     products = CallProduct.objects.filter(call=call)
 
-    context['orders'] = orders
-    context['call'] = call
-    context['products'] = products
+    context["orders"] = orders
+    context["call"] = call
+    context["products"] = products
     return render(request, template_name, context)
 
 
 @login_required
 @staff_required
 def index_admin(request):
-    template_name = 'index-admin.html'
+    template_name = "index-admin.html"
     context = {}
 
     calls = Call.objects.filter(active=True)
-    orders = Order.objects.filter(status='pending')
+    orders = Order.objects.filter(status="pending")
 
-    context['calls'] = calls
-    context['orders'] = orders
+    context["calls"] = calls
+    context["orders"] = orders
     return render(request, template_name, context)
 
 
 @login_required
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect("login")
