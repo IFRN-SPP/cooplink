5131ea94c069ecf0f7f98898a321a0e94f7f7f72
Ana Barbosa <anael.noki@gmail.com>
2024-11-25 09:08:08 -0300

    feat: adiciona nova model Cooperative

--- app/models.py
+++ app/models.py
@@ -2,6 +2,39 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser
 
+from django.db import models
+
+
+class Cooperative(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Nome da Cooperativa")
+    logo = models.CharField(
+        max_length=255,
+        verbose_name="Caminho para a Logo da Cooperativa",
+        null=True,
+        blank=True,
+        help_text="Caminho relativo ao arquivo dentro de static/",
+    )
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if Cooperative.objects.exists() and not self.pk:
+            raise ValueError("Já existe uma cooperativa cadastrada. Só pode haver uma.")
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def update_or_create(cls, name, logo_path=None):
+        cooperative, created = cls.objects.update_or_create(
+            defaults={"logo": logo_path},
+            name=name,
+        )
+        return cooperative, created
+
+    @classmethod
+    def get_instance(cls):
+        return cls.objects.first()
+
 
 class Institution(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nome da Instituição")
@@ -11,7 +44,7 @@ class Institution(models.Model):
         return self.name
 
     class Meta:
-        ordering = ['-id']
+        ordering = ["-id"]
 
     @property
     def users(self):
@@ -29,7 +62,9 @@ class Institution(models.Model):
 class UserProfile(AbstractUser):
     username = models.EmailField(verbose_name="Email", max_length=150, unique=True)
     first_name = models.CharField(verbose_name="Nome Completo", max_length=150)
-    institution = models.ForeignKey(Institution, on_delete=models.PROTECT, null=True, verbose_name="Instituição")
+    institution = models.ForeignKey(
+        Institution, on_delete=models.PROTECT, null=True, verbose_name="Instituição"
+    )
 
     REQUIRED_FIELDS = ["institution", "first_name"]
 
@@ -37,33 +72,43 @@ class UserProfile(AbstractUser):
         return self.get_full_name()
 
     class Meta:
-        ordering = ['-id']
+        ordering = ["-id"]
 
 
 class Call(models.Model):
     number = models.CharField(max_length=50, verbose_name="Número")
-    institution = models.ForeignKey(Institution, on_delete=models.PROTECT, verbose_name="Instituição")
-    start = models.DateField(auto_now=False, auto_now_add=False, verbose_name="Data de Início")
-    end = models.DateField(auto_now=False, auto_now_add=False, verbose_name="Data de Término")
+    institution = models.ForeignKey(
+        Institution, on_delete=models.PROTECT, verbose_name="Instituição"
+    )
+    start = models.DateField(
+        auto_now=False, auto_now_add=False, verbose_name="Data de Início"
+    )
+    end = models.DateField(
+        auto_now=False, auto_now_add=False, verbose_name="Data de Término"
+    )
     active = models.BooleanField(default=False, verbose_name="Situação")
 
     def __str__(self):
-        return f'Chamada - {self.number} de {self.institution.name}'
+        return f"Chamada - {self.number} de {self.institution.name}"
 
     def clean(self):
         if self.active:
-            active_calls = Call.objects.filter(institution=self.institution, active=True)
+            active_calls = Call.objects.filter(
+                institution=self.institution, active=True
+            )
             if self.pk:
                 active_calls = active_calls.exclude(pk=self.pk)
             if active_calls.exists():
-                raise ValidationError("Já existe uma chamada ativa para esta instituição.")
+                raise ValidationError(
+                    "Já existe uma chamada ativa para esta instituição."
+                )
 
     def save(self, *args, **kwargs):
         self.full_clean()
         super().save(*args, **kwargs)
 
     class Meta:
-        ordering = ['-id']
+        ordering = ["-id"]
 
     @property
     def products(self):
@@ -71,55 +116,70 @@ class Call(models.Model):
             products = CallProduct.objects.filter(call=self.id)
             return products
 
+
 class Product(models.Model):
     CHOICES = [
-        ('KG', 'KG'),
-        ('UNI', 'UNI'),
-        ('BDJ', 'BDJ'),
-        ('L', 'L'),
+        ("KG", "KG"),
+        ("UNI", "UNI"),
+        ("BDJ", "BDJ"),
+        ("L", "L"),
     ]
     name = models.CharField(max_length=100, verbose_name="Nome do Produto")
-    unit = models.CharField(max_length=3, choices=CHOICES, verbose_name="Unidade de Medida")
+    unit = models.CharField(
+        max_length=3, choices=CHOICES, verbose_name="Unidade de Medida"
+    )
 
     def __str__(self):
         return self.name
 
     class Meta:
-        ordering = ['-id']
+        ordering = ["-id"]
 
 
 class CallProduct(models.Model):
-    call = models.ForeignKey(Call, on_delete=models.CASCADE, related_name='product', verbose_name="Chamada")
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='call', verbose_name="Produto")
+    call = models.ForeignKey(
+        Call, on_delete=models.CASCADE, related_name="product", verbose_name="Chamada"
+    )
+    product = models.ForeignKey(
+        Product, on_delete=models.PROTECT, related_name="call", verbose_name="Produto"
+    )
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Preço")
     balance = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Saldo")
 
     def __str__(self):
-        return f'{self.product} {self.price} - {self.call.number}'
+        return f"{self.product} {self.price} - {self.call.number}"
 
     class Meta:
-        ordering = ['-id']
+        ordering = ["-id"]
 
 
 class Order(models.Model):
     CHOICES = [
-        ('pending', 'Pendente'),
-        ('approved', 'Aprovado'),
-        ('denied', 'Negado'),
-        ('delivered', 'Entregue'),
+        ("pending", "Pendente"),
+        ("approved", "Aprovado"),
+        ("denied", "Negado"),
+        ("delivered", "Entregue"),
     ]
 
-    user = models.ForeignKey(UserProfile, on_delete=models.PROTECT, verbose_name="Autor")
-    institution = models.ForeignKey(Institution, on_delete=models.PROTECT, verbose_name="Instituição")
+    user = models.ForeignKey(
+        UserProfile, on_delete=models.PROTECT, verbose_name="Autor"
+    )
+    institution = models.ForeignKey(
+        Institution, on_delete=models.PROTECT, verbose_name="Instituição"
+    )
     call = models.ForeignKey(Call, on_delete=models.PROTECT, verbose_name="Chamada")
-    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name="Timestamp")
-    status = models.CharField(max_length=10, choices=CHOICES, default='pending', verbose_name="Situação")
+    timestamp = models.DateTimeField(
+        auto_now=False, auto_now_add=True, verbose_name="Timestamp"
+    )
+    status = models.CharField(
+        max_length=10, choices=CHOICES, default="pending", verbose_name="Situação"
+    )
 
     def __str__(self):
-        return f'Pedido de {self.institution} (Chamada {self.call.number}) feito por {self.user}'
+        return f"Pedido de {self.institution} (Chamada {self.call.number}) feito por {self.user}"
 
     class Meta:
-        ordering = ['-timestamp']
+        ordering = ["-timestamp"]
 
     @property
     def products(self):
@@ -129,10 +189,9 @@ class Order(models.Model):
 
     @property
     def available_products(self):
-        if (self.status == 'approved') or (self.status == 'delivered') :
+        if (self.status == "approved") or (self.status == "delivered"):
             products = OrderedProduct.objects.filter(
-                order=self,
-                status__in=('parcial', 'available')
+                order=self, status__in=("parcial", "available")
             )
             return products
 
@@ -142,37 +201,56 @@ class Order(models.Model):
         if self.id:
             products = self.available_products
             for product in products:
-                total_price += float(product.get_quantity_price.replace(',', '.'))
-        return "{:.2f}".format(total_price).replace('.', ',') if total_price else None
+                total_price += float(product.get_quantity_price.replace(",", "."))
+        return "{:.2f}".format(total_price).replace(".", ",") if total_price else None
 
     @property
     def request_products(self):
-        if (self.status == 'approved') or (self.status == 'pending') :
+        if (self.status == "approved") or (self.status == "pending"):
             products = OrderedProduct.objects.filter(
-                order=self,
-                status__in=('parcial', 'available')
+                order=self, status__in=("parcial", "available")
             )
             return products
 
 
 class OrderedProduct(models.Model):
     CHOICES = [
-        ('parcial', 'Parcial'),
-        ('available', 'Disponível'),
-        ('denied', 'Negado'),
+        ("parcial", "Parcial"),
+        ("available", "Disponível"),
+        ("denied", "Negado"),
     ]
 
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='call_products', verbose_name="Pedido")
-    call_product = models.ForeignKey(CallProduct, on_delete=models.PROTECT, related_name='order', verbose_name="Produto da Chamada")
-    ordered_quantity = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Quant. Pedida")
-    available_quantity = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name="Quant. Disponível")
-    status = models.CharField(max_length=10, choices=CHOICES, default='available', verbose_name="Situação")
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="call_products",
+        verbose_name="Pedido",
+    )
+    call_product = models.ForeignKey(
+        CallProduct,
+        on_delete=models.PROTECT,
+        related_name="order",
+        verbose_name="Produto da Chamada",
+    )
+    ordered_quantity = models.DecimalField(
+        max_digits=7, decimal_places=2, verbose_name="Quant. Pedida"
+    )
+    available_quantity = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        null=True,
+        blank=True,
+        verbose_name="Quant. Disponível",
+    )
+    status = models.CharField(
+        max_length=10, choices=CHOICES, default="available", verbose_name="Situação"
+    )
 
     def __str__(self):
-        return f'({self.order}) ({self.call_product}) ({self.ordered_quantity})'
+        return f"({self.order}) ({self.call_product}) ({self.ordered_quantity})"
 
     class Meta:
-        ordering = ['-id']
+        ordering = ["-id"]
 
     @property
     def get_quantity_price(self):
@@ -182,5 +260,9 @@ class OrderedProduct(models.Model):
             quantity = float(self.ordered_quantity)
             if self.available_quantity:
                 quantity = float(self.available_quantity)
-            quantity_price = price*quantity
-        return "{:.2f}".format(quantity_price).replace('.', ',') if quantity_price is not None else None
+            quantity_price = price * quantity
+        return (
+            "{:.2f}".format(quantity_price).replace(".", ",")
+            if quantity_price is not None
+            else None
+        )
